54a503ffd5316d014abfa011f62ad9e49201f9ed
Ali107 <Alialawadhi2014bh@gmail.com>
2024-02-16 04:25:42 +0300

    Added Colors.Lerp() in Utilities.

--- SpriteX Engine/EngineContents/Utilities.cs
+++ SpriteX Engine/EngineContents/Utilities.cs
@@ -75,6 +75,115 @@ namespace SpriteX_Engine.EngineContents
         }
 
         /// <summary>
+        /// class that stores functions that are related to numbers
+        /// </summary>
+        public static class Numbers
+        {
+            /// <summary>
+            /// calculates the distance between two single numbers
+            /// </summary>
+            /// <param name="num1"></param>
+            /// <param name="num2"></param>
+            /// <returns></returns>
+            public static float Distance1D(float num1, float num2)
+            {
+                return MathF.Sqrt(MathF.Pow(num1 - num2, 2));
+            }
+
+            /// <summary>
+            /// Will return the average of numbers inside an array
+            /// </summary>
+            /// <param name="floatArray"></param>
+            /// <returns></returns>
+            public static float AverageNum(float[] floatArray)
+            {
+                float value = 0.0f;
+                for (int i = 0; i < floatArray.Length; i++)
+                    value += floatArray[i];
+
+                return value / floatArray.Length;
+            }
+
+            /// <summary>
+            /// Converts degrees to radians
+            /// </summary>
+            /// <param name="degree"></param>
+            /// <returns></returns>
+            public static float DegreeToRad(float degree)
+            {
+                return (degree * MathF.PI) / 180.0f;
+            }
+
+            /// <summary>
+            /// Converts radian to degrees
+            /// </summary>
+            /// <param name="radian"></param>
+            /// <returns></returns>
+            public static float RadToDegree(float radian)
+            {
+                return (radian * 180.0f) / MathF.PI;
+            }
+
+            /// <summary>
+            /// Fixes the value number between the minimum and maximum
+            /// </summary>
+            /// <param name="val"></param>
+            /// <param name="min"></param>
+            /// <param name="max"></param>
+            /// <returns></returns>
+            public static float ClampN(float val, float min, float max)
+            {
+                return val > max ? max : val < min ? min : val;
+            }
+
+            /// <summary>
+            /// Returns the largest number in an array of floats
+            /// </summary>
+            /// <param name="floatArray"></param>
+            /// <returns></returns>
+            public static float MaxVal(float[] floatArray)
+            {
+                float max = 0;
+                for (int i = 0; i < floatArray.Length; i++)
+                    if (floatArray[i] > max)
+                    {
+                        max = floatArray[i];
+                    }
+
+                return max;
+            }
+
+            /// <summary>
+            /// Returns the smallest number in an array of floats
+            /// </summary>
+            /// <param name="floatArray"></param>
+            /// <returns></returns>
+            public static float MinVal(float[] floatArray)
+            {
+                float min = floatArray[0];
+                for (int i = 0; i < floatArray.Length; i++)
+                    if (floatArray[i] < min)
+                    {
+                        min = floatArray[i];
+                    }
+
+                return min;
+            }
+
+            /// <summary>
+            /// Lerps two numbers based on "alpha"
+            /// </summary>
+            /// <param name="a"></param>
+            /// <param name="b"></param>
+            /// <param name="alpha"></param>
+            /// <returns></returns>
+            public static float Lerp(float a, float b, float alpha)
+            {
+                return a * (1 - alpha) + b * alpha;
+            }
+        }
+
+        /// <summary>
         /// class that stores functions that are related to Vector2
         /// </summary>
         public static class Vec2D
@@ -210,112 +319,18 @@ namespace SpriteX_Engine.EngineContents
             }
         }
 
-        /// <summary>
-        /// class that stores functions that are related to numbers
-        /// </summary>
-        public static class Numbers
+        public static class Colors
         {
             /// <summary>
-            /// calculates the distance between two single numbers
-            /// </summary>
-            /// <param name="num1"></param>
-            /// <param name="num2"></param>
-            /// <returns></returns>
-            public static float Distance1D(float num1, float num2)
-            {
-                return MathF.Sqrt(MathF.Pow(num1 - num2, 2));
-            }
-
-            /// <summary>
-            /// Will return the average of numbers inside an array
-            /// </summary>
-            /// <param name="floatArray"></param>
-            /// <returns></returns>
-            public static float AverageNum(float[] floatArray)
-            {
-                float value = 0.0f;
-                for (int i = 0; i < floatArray.Length; i++)
-                    value += floatArray[i];
-
-                return value / floatArray.Length;
-            }
-
-            /// <summary>
-            /// Converts degrees to radians
-            /// </summary>
-            /// <param name="degree"></param>
-            /// <returns></returns>
-            public static float DegreeToRad(float degree)
-            {
-                return (degree * MathF.PI) / 180.0f;
-            }
-
-            /// <summary>
-            /// Converts radian to degrees
-            /// </summary>
-            /// <param name="radian"></param>
-            /// <returns></returns>
-            public static float RadToDegree(float radian)
-            {
-                return (radian * 180.0f) / MathF.PI;
-            }
-
-            /// <summary>
-            /// Fixes the value number between the minimum and maximum
-            /// </summary>
-            /// <param name="val"></param>
-            /// <param name="min"></param>
-            /// <param name="max"></param>
-            /// <returns></returns>
-            public static float ClampN(float val, float min, float max)
-            {
-                return val > max ? max : val < min ? min : val;
-            }
-
-            /// <summary>
-            /// Returns the largest number in an array of floats
-            /// </summary>
-            /// <param name="floatArray"></param>
-            /// <returns></returns>
-            public static float MaxVal(float[] floatArray)
-            {
-                float max = 0;
-                for (int i = 0; i < floatArray.Length; i++)
-                    if (floatArray[i] > max)
-                    {
-                        max = floatArray[i];
-                    }
-
-                return max;
-            }
-
-            /// <summary>
-            /// Returns the smallest number in an array of floats
-            /// </summary>
-            /// <param name="floatArray"></param>
-            /// <returns></returns>
-            public static float MinVal(float[] floatArray)
-            {
-                float min = floatArray[0];
-                for (int i = 0; i < floatArray.Length; i++)
-                    if (floatArray[i] < min)
-                    {
-                        min = floatArray[i];
-                    }
-
-                return min;
-            }
-
-            /// <summary>
-            /// Lerps two numbers based on "alpha"
+            /// Lerps two Colors based on "alpha"
             /// </summary>
             /// <param name="a"></param>
             /// <param name="b"></param>
             /// <param name="alpha"></param>
             /// <returns></returns>
-            public static float Lerp(float a, float b, float alpha)
+            public static Color4 Lerp(Color4 a, Color4 b, float alpha)
             {
-                return a * (1 - alpha) + b * alpha;
+                return new Color4(Numbers.Lerp(a.R, b.R, alpha), Numbers.Lerp(a.G, b.G, alpha), Numbers.Lerp(a.B, b.B, alpha), Numbers.Lerp(a.A, b.A, alpha));
             }
         }
 
