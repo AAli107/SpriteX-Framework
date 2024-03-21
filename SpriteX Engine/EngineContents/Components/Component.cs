a2db5894f491bae45b70e6f5ce5722ab1e13ba21
Ali107 <Alialawadhi2014bh@gmail.com>
2024-03-21 21:32:11 +0300

    Added Component abstract class.

---
+++ SpriteX Engine/EngineContents/Components/Component.cs
@@ -0,0 +1,15 @@
+﻿namespace SpriteX_Engine.EngineContents.Components
+{
+    public abstract class Component
+    {
+        public GameObject parent;
+
+        public Component(GameObject parent)
+        {
+            this.parent = parent;
+        }
+
+        public virtual void GameTick(MainWindow win) { }
+        public virtual void RenderTick(MainWindow win, gfx gfx) { }
+    }
+}
