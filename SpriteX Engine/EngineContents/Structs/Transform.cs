6f919b2c48d30ba32c30c2c4e9a7e1cec8f03e4a
Ali107 <Alialawadhi2014bh@gmail.com>
2024-03-22 01:47:57 +0300

    Created Transform struct

---
+++ SpriteX Engine/EngineContents/Structs/Transform.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK.Mathematics;
+
+namespace SpriteX_Engine.EngineContents.Structs
+{
+    public struct Transform
+    {
+        public Vector2 position = new Vector2(0, 0);
+        public Vector2 scale = new Vector2(1, 1);
+
+        public Transform(Vector2 position, Vector2 scale)
+        {
+            this.position = position;
+            this.scale = scale;
+        }
+    }
+}
