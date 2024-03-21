a1ab160a8b38977872f397b14078247306660b33
Ali107 <Alialawadhi2014bh@gmail.com>
2024-03-22 01:48:32 +0300

    Added default constructor for Transform

--- SpriteX Engine/EngineContents/Structs/Transform.cs
+++ SpriteX Engine/EngineContents/Structs/Transform.cs
@@ -7,6 +7,7 @@ namespace SpriteX_Engine.EngineContents.Structs
         public Vector2 position = new Vector2(0, 0);
         public Vector2 scale = new Vector2(1, 1);
 
+        public Transform() { }
         public Transform(Vector2 position, Vector2 scale)
         {
             this.position = position;
