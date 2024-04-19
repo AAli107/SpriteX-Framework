b39ad300dfc63d91a821039f8b0545f4840df4ff
Ali107 <Alialawadhi2014bh@gmail.com>
2024-04-19 16:34:55 +0300

    Added summary for each method in CharacterBase class.

--- SpriteX Engine/EngineContents/Assets/GameObjects/CharacterBase.cs
+++ SpriteX Engine/EngineContents/Assets/GameObjects/CharacterBase.cs
@@ -11,8 +11,17 @@ namespace SpriteX_Engine.EngineContents.Assets.GameObjects
         private uint iframes = 0;
         private Vector2 spawnpoint;
 
+        /// <summary>
+        /// Determines whether the character is immune to damage or not
+        /// </summary>
         public bool Invincibility { get; set; }
+        /// <summary>
+        /// Returns true when hitPoints is 0, aka when the character dies
+        /// </summary>
         public bool IsDead { get { return hitPoints <= 0; } }
+        /// <summary>
+        /// Returns whether the character is currently invulnerable to attacks determined by iframes and Invincibility
+        /// </summary>
         public bool IsInvulnerable { get { return iframes > 0 || Invincibility; } }
 
         public CharacterBase(Vector2 position) : base(position) 
@@ -33,37 +42,76 @@ namespace SpriteX_Engine.EngineContents.Assets.GameObjects
                 spawnpoint = GetPosition();
         }
 
+        /// <summary>
+        /// Respawns the character by moving it to its spawnpoint and refilling the hitpoints to full
+        /// </summary>
         public void Respawn()
         {
             CharacterBase_Spawn(null, null);
             SetPosition(spawnpoint);
         }
 
+        /// <summary>
+        /// Executed when the character dies / hitpoints reaches 0
+        /// </summary>
+        /// <param name="damageType"></param>
         public virtual void DeathSequence(DamageType damageType) { }
 
+        /// <summary>
+        /// Sets the current HP of the character
+        /// </summary>
+        /// <param name="hp"></param>
         public void SetHP(float hp)
         {
             hitPoints = hp; 
             if (IsDead) DeathSequence(DamageType.None);
         }
 
+        /// <summary>
+        /// Sets the maximum HP of the character
+        /// </summary>
+        /// <param name="maxHP"></param>
         public void SetMaxHP(float maxHP)
         {
             if (maxHP > 0) { maxHitPoints = maxHP; }
         }
 
+        /// <summary>
+        /// Sets where the character will respawn by changing its spawnpoint
+        /// </summary>
+        /// <param name="spawnpoint"></param>
+        /// <param name="willRespawn"></param>
         public void SetSpawnpoint(Vector2 spawnpoint, bool willRespawn = false)
         {
             this.spawnpoint = spawnpoint;
             if (willRespawn) Respawn();
         }
 
+        /// <summary>
+        /// Returns current HP of the character
+        /// </summary>
+        /// <returns></returns>
         public float GetHP() { return hitPoints; }
 
+        /// <summary>
+        /// returns maximum HP of the character
+        /// </summary>
+        /// <returns></returns>
         public float GetMaxHP() { return maxHitPoints; }
 
+        /// <summary>
+        /// returns character spawnpoint
+        /// </summary>
+        /// <returns></returns>
         public Vector2 GetSpawnpoint() { return spawnpoint; }
 
+        /// <summary>
+        /// Deducts Character's hitpoints
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="iframes"></param>
+        /// <param name="damageType"></param>
+        /// <returns></returns>
         public bool DealDamage(float amount, uint iframes = 60, DamageType damageType = DamageType.Generic)
         {
             if (!IsDead && !IsInvulnerable)
@@ -75,6 +123,11 @@ namespace SpriteX_Engine.EngineContents.Assets.GameObjects
             } else return false;
         }
 
+        /// <summary>
+        /// Increases Character's hitpoints
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
         public bool Heal(float amount)
         {
             if (!IsDead)
