3bfda24cbf96bc8618475abbad3dd41c75697f88
Ali107 <Alialawadhi2014bh@gmail.com>
2024-04-21 15:43:54 +0300

    Create TopDownCharacter.cs

---
+++ SpriteX Engine/EngineContents/Assets/GameObjects/TopDownCharacter.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpriteX_Engine.EngineContents.Assets.GameObjects
+{
+    public class TopDownCharacter : CharacterBase
+    {
+        public TopDownCharacter(Vector2 position) : base(position)
+        {
+        }
+    }
+}
