a4ca7dc82e568ebdf7a525c1ac85aabc0cc84fd5
Ali107 <Alialawadhi2014bh@gmail.com>
2024-04-22 01:33:30 +0300

    Added physics and collision to TopDownCharacter.

--- SpriteX Engine/EngineContents/Assets/GameObjects/TopDownCharacter.cs
+++ SpriteX Engine/EngineContents/Assets/GameObjects/TopDownCharacter.cs
@@ -1,11 +1,20 @@
 ﻿using OpenTK.Mathematics;
+using SpriteX_Engine.EngineContents.Components;
 
 namespace SpriteX_Engine.EngineContents.Assets.GameObjects
 {
     public class TopDownCharacter : CharacterBase
     {
+        PhysicsComponent pc;
+        ColliderComponent cc;
+
         public TopDownCharacter(Vector2 position) : base(position)
         {
+            pc = AddComponent<PhysicsComponent>();
+            cc = AddComponent<ColliderComponent>();
+
+            pc.gravityEnabled = false;
+            pc.isAirborne = false;
         }
     }
 }
