f6d58b6c8f53ee7c47f26f4597cffdc7ee02d281
Ali107 <Alialawadhi2014bh@gmail.com>
2024-09-14 23:24:41 +0300

    Update InitialGameWindowConfig.cs

--- SpriteX Engine/FrameworkContents/InitialGameWindowConfig.cs
+++ SpriteX Engine/FrameworkContents/InitialGameWindowConfig.cs
@@ -9,16 +9,16 @@ namespace SpriteX_Framework.FrameworkContents
         /* --- Initial Game Window Parameters --- */
 
         public static readonly string Title = "SpriteX Game"; // Window Title
-        public static readonly Vector2i ClientSize = new Vector2i(1280, 720); // Default Window Resolution when not in fullscreen
+        public static readonly Vector2i ClientSize = new (1280, 720); // Default Window Resolution when not in fullscreen
         public static readonly (int numerator, int denominator) AspectRatio = (16, 9); // Window Aspect Ratio
         public static readonly WindowBorder WindowBorder = WindowBorder.Resizable; // Window Border type
-        public static readonly WindowState WindowState = WindowState.Maximized; // Decides window state (can be used to set fullscreen) 
+        public static readonly WindowState WindowState = WindowState.Normal; // Decides window state (can be used to set fullscreen) 
         public static readonly double UpdateFrequency = 0; // Window Framerate (setting to 0 will unlock FPS if VSync is off)
         public static readonly double fixedFrameTime = 60; // How many times per second the game updates
         public static readonly VSyncMode VSync = VSyncMode.Off; // Control the window's VSync
         public static readonly Color bgColor = Color.Black; // Controls the windows background color
         public static readonly bool allowAltEnter = true; // Controls whether you can toggle fullscreen when pressing Alt+Enter
-        public static readonly bool showDebugHitbox = true; // Controls whether to show all GameObjects' hitboxes
+        public static readonly bool showDebugHitbox = false; // Controls whether to show all GameObjects' collider components
         public static readonly bool showStats = true; // Displays FPS and UpdateTime(ms) Stat
         public static readonly Font font = Font.defaultFont; // Contains game font
         public static readonly GameLevelScript startLevel = new SampleLevel(); // The Level to load when game launches
