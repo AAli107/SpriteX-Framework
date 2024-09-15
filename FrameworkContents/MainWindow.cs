1ada5b5751f9cb87c6f64d5a40fab8d0e29bd62a
Ali107 <Alialawadhi2014bh@gmail.com>
2024-09-15 22:01:59 +0300

    Update MainWindow.cs

--- FrameworkContents/MainWindow.cs
+++ FrameworkContents/MainWindow.cs
@@ -87,8 +87,13 @@ namespace SpriteX_Framework.FrameworkContents
         /// Stores the game world
         /// </summary>
         public World world { get; private set; }
-
+        /// <summary>
+        /// The position of the mouse in the window
+        /// </summary>
         public Vector2d mouseScreenPos { get { return MousePosition / (ClientSize / new Vector2d(1920, 1080)); } }
+        /// <summary>
+        /// The position of the mouse in the world
+        /// </summary>
         public Vector2d mouseWorldPos { get { return (MousePosition / (ClientSize / new Vector2d(1920, 1080))) + new Vector2d(GetWorldCamera().camPos.X - 960, GetWorldCamera().camPos.Y - 540); } }
 
         protected override void OnLoad()
