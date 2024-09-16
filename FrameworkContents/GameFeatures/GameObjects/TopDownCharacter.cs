f04d678a1501bb5625afecc06df9c13eaa642341
Ali107 <Alialawadhi2014bh@gmail.com>
2024-09-16 15:34:29 +0300

    Added Missing summary in TopDownCharacter class.

--- FrameworkContents/GameFeatures/GameObjects/TopDownCharacter.cs
+++ FrameworkContents/GameFeatures/GameObjects/TopDownCharacter.cs
@@ -17,24 +17,40 @@ namespace SpriteX_Framework.FrameworkContents.GameFeatures.GameObjects
             pc.isAirborne = false;
         }
 
+        /// <summary>
+        /// Causes the Character to move forward
+        /// </summary>
+        /// <param name="speed"></param>
         public void MoveForward(float speed = 1f)
         {
             if (pc != null)
                 pc.AddVelocity(ForwardDirection * speed);
         }
 
+        /// <summary>
+        /// Causes the Character to strafe right. (negative speed will make it strafe left)
+        /// </summary>
+        /// <param name="speed"></param>
         public void StrafeRight(float speed = 1f)
         {
             if (pc != null)
                 pc.AddVelocity(RightDirection * speed);
         }
 
+        /// <summary>
+        /// Allows you to set whether to enable or disable character physics
+        /// </summary>
+        /// <param name="simulatePhysics"></param>
         public void SetSimulatePhysics(bool simulatePhysics)
         {
             if (pc != null)
                 pc.isEnabled = simulatePhysics;
         }
 
+        /// <summary>
+        /// Returns whether the character is simulating physics or not
+        /// </summary>
+        /// <returns></returns>
         public bool IsSimulatingPhysics()
         {
             return pc != null && pc.isEnabled;
